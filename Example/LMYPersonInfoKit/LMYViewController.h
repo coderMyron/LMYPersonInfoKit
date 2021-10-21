71f14b7283438e6163ef6ba4dfe9d0a5d38e7ee9
coderMyron <244270327@qq.com>
2021-10-21 16:47:45 +0800

    Initial commit

---
+++ Example/LMYPersonInfoKit/LMYViewController.h
@@ -0,0 +1,13 @@
+//
+//  LMYViewController.h
+//  LMYPersonInfoKit
+//
+//  Created by coderMyron on 10/21/2021.
+//  Copyright (c) 2021 coderMyron. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LMYViewController : UIViewController
+
+@end
